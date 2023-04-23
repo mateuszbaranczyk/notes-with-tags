3d24dd0c5aa77c290640c4e4f1415eaa283ec08f
kundzior <grzybek2413@gmail.com>
2023-04-23 16:11:08 +0200

    new func vegetables

--- app/main.py
+++ app/main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/vegetables/{name}")
+def get_vegetables(name: str):
+    vegetables = ["onion", "garlic", "potato"]
+    if name in vegetables:
+        return {"vege_name": name}
+    else:
+        return {"error": f"There is no {name} in the store"}
