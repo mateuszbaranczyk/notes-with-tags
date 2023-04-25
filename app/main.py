46489dd4eaf37d8f6be11cba9accf18f90ed1eaa
kundzior <grzybek2413@gmail.com>
2023-04-25 21:02:42 +0200

    params metadata

--- app/main.py
+++ app/main.py
@@ -28,19 +28,27 @@ async def create_item(item: Item):
     return item
 
 
-#query parameter with max len 50 and default value None as not required 
-@app.get("/read_items/")
-async def read_items(query_param: Annotated[str | None, Query(max_length=8)] = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+# query parameter with max len 50 and default value None as not required
+@app.get("/read_item/")
+async def read_item(query_param: Annotated[str | None, Query(max_length=8)] = None):
+    results = {"item": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if query_param:
         results.update({"q": query_param})
     return results
 
 
-#use Annotated[list[str] to pass more items in list like /read_more_items/?q=foo&q=bar
+# use Annotated[list[str] to pass more items in list like /read_more_items/?q=foo&q=bar
+# @app.get("/read_more_items/")
+# async def read_more_items(query_params: Annotated[list[str] | None, Query()] = None):
+#     query_items = {"q": query_params}
+#     return query_items
+#
+# you can create params metadata like this 
 @app.get("/read_more_items/")
-async def read_more_items(query_params: Annotated[list[str] | None, Query()] = None):
-    query_items = {"q": query_params}
-    return query_items
-
-
+async def read_more_items(
+    query_params: Annotated[
+        list[str], Query(title="Items", description="Pass list of products", alias="Products")
+    ]
+):
+    items = {"items": query_params}
+    return items
