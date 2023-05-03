bf58e9fde01f5bf6983bfbb4e8a2d3b3dcb5a3a5
kundzior <grzybek2413@gmail.com>
2023-05-03 17:29:33 +0200

    some fixes

--- app/main.py
+++ app/main.py
@@ -71,9 +71,10 @@ async def read_item_id(
 # without Body it would be query parameter
 # Body also has all the same extra validation and metadata parameters as Query,Path and others you will see later.
 @app.get("/item_for_user/")
-async def item_for_user(item: Item,  user_name: User, importanace: Annotated[int, Body()]):
+async def item_for_user(item: Item, user_name: User, importanace: Annotated[int, Body()]):
     result = {"item": item, "user": user_name, "importanace": importanace}
     return result
+
 
 # Cookies and Headers are used the same way as below
 @app.get("/item_with_ads/")
