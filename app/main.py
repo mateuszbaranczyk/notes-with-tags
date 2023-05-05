fe1ca87765e8a05c61c4878691e0302d3e96a50b
kundzior <grzybek2413@gmail.com>
2023-05-05 19:25:54 +0200

    black

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 database_result = {"title": "note_1", "content": "note_content", "tags": ["test_1", "test_2"]}
 tags_from_db = ["tag1", "tag2", "tag2"]
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -22,6 +23,7 @@ async def create_note(note: Annotated[models.Note, Body(embeded=True)]):
 @app.get("/note/{title}", response_model=models.Note)
 async def get_note(title: str):
     return database_result
+
 
 @app.get("/tags/")
 async def get_tags() -> set[str]:
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
 import json
 
 from fastapi.testclient import TestClient
-
-from app.main import app
 from requests import Response
 
+from app.main import app
+
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
@@ -17,7 +18,8 @@ def test_create_note():
     response = client.put("/create_note/", json=data)
     _assert_response(result=data, response=response, status_code=200)
 
-def _assert_response(result:dict, response: Response, status_code: int):
+
+def _assert_response(result: dict, response: Response, status_code: int):
     response_body = json.loads(response.content)
     assert response.status_code == status_code
     assert response_body == result
@@ -35,12 +37,17 @@ def test_create_note_with_img():
 
 
 def test_get_note():
-    result = {"title": "note_1", "content": "note_content", "tags": ["test_1", "test_2"], "image": None}
+    result = {
+        "title": "note_1",
+        "content": "note_content",
+        "tags": ["test_1", "test_2"],
+        "image": None,
+    }
     response = client.get(f"/note/{result['title']}")
     _assert_response(result=result, response=response, status_code=200)
+
 
 def test_get_tags():
     result = ["tag1", "tag2"]
     response = client.get("/tags/")
     _assert_response(result=result, response=response, status_code=200)
-
