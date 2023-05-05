8c0f3339dbea9f50517714c90f05da9aa5839fa0
kundzior <grzybek2413@gmail.com>
2023-05-05 19:17:03 +0200

    mod tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,9 +3,9 @@ import json
 from fastapi.testclient import TestClient
 
 from app.main import app
+from requests import Response
 
 client = TestClient(app)
-
 
 def test_root():
     response = client.get("/")
@@ -15,9 +15,12 @@ def test_root():
 def test_create_note():
     data = {"title": "title", "content": "test note", "tags": ["tag1", "tag2"], "image": None}
     response = client.put("/create_note/", json=data)
+    _assert_response(result=data, response=response, status_code=200)
+
+def _assert_response(result:dict, response: Response, status_code: int):
     response_body = json.loads(response.content)
-    assert response.status_code == 200
-    assert response_body == data
+    assert response.status_code == status_code
+    assert response_body == result
 
 
 def test_create_note_with_img():
@@ -28,23 +31,17 @@ def test_create_note_with_img():
         "image": {"title": "photo_title", "url": "https://test.pl"},
     }
     response = client.put("/create_note/", json=data)
-    response_body = json.loads(response.content)
-    assert response.status_code == 200
-    assert response_body == data
+    _assert_response(result=data, response=response, status_code=200)
 
 
 def test_get_note():
     title = "note_1"
     result = {"title": "note_1", "content": "note_content", "tags": ["test_1", "test_2"], "image": None}
     response = client.get(f"/note/{title}")
-    response_body = json.loads(response.content)
-    assert response.status_code == 200
-    assert response_body == result
+    _assert_response(result=result, response=response, status_code=200)
 
 def test_get_tags():
     result = ["tag1", "tag2"]
     response = client.get("/tags/")
-    response_body = json.loads(response.content)
-    assert response.status_code == 200
-    assert response_body == result
+    _assert_response(result=result, response=response, status_code=200)
 
