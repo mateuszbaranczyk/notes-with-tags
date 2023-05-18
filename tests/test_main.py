a7c8c08a7d9e3c0918904ba5d9652b1c4a320618
kundzior <grzybek2413@gmail.com>
2023-05-18 20:25:17 +0200

    test db config

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,32 @@ import json
 
 from fastapi.testclient import TestClient
 from requests import Response
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database.db_config import Base, get_db
 from app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
 
 client = TestClient(app)
 
