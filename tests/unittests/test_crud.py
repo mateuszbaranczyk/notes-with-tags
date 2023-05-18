c17178586b226d3dbbd715c1590b64ab7d225a39
kundzior <grzybek2413@gmail.com>
2023-05-18 18:13:17 +0200

    created testing db

--- tests/unittests/test_crud.py
+++ tests/unittests/test_crud.py
@@ -1,5 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from app.database import crud
+from app.database.db_config import Base, get_db
 from app.database.db_models import ImageRead, ImageWrite, NoteRead, NoteWrite
+from app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 
 def test_create_note():
