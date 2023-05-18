5385837ddc0847f9348dfef795e7df8cadd57bae
kundzior <grzybek2413@gmail.com>
2023-05-18 17:58:13 +0200

    test for crud

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,13 +1,17 @@
-from app.database.db_models import ImageWrite, ImageRead, NoteRead, NoteWrite
+from app.database.db_models import ImageRead, ImageWrite, NoteRead, NoteWrite
+
 
 def create_note():
     pass
 
+
 def create_image():
     pass
 
+
 def read_note():
     pass
+
 
 def read_image():
     pass
--- tests/unittests/test_crud.py
+++ tests/unittests/test_crud.py
@@ -1,9 +1,8 @@
 from app.database import crud
+from app.database.db_models import ImageRead, ImageWrite, NoteRead, NoteWrite
 
-from app.database.db_models import ImageWrite, ImageRead, NoteRead, NoteWrite
 
 def test_create_note():
     note_data = {}
     result = crud.create_note()
     assert result.title == note_data["title"]
-    
