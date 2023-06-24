4d58bc76c79904e0c12049a04aa29fc77f236d01
kundzior <grzybek2413@gmail.com>
2023-06-24 15:02:35 +0200

    splited tests

--- tests/test_main/test_main.py
+++ tests/test_main/test_main.py
@@ -1,12 +1,3 @@
-from tests.utils import create_note
-
-
 def test_root(client):
     response = client.get("/")
-    assert response.status_code == 200
-
-
-def test_get_tags(client):
-    create_note(client)
-    response = client.get("/tags/")
     assert response.status_code == 200
